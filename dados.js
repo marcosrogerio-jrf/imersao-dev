191635a5b37cab242c81f690763f3e3f7b9bb565
Marcos Rogério Ferreira <30937175+marcosrogerio-jrf@users.noreply.github.com>
2024-09-07 10:16:08 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Rodrigues de Andrade (Guarulhos, 8 de maio de 1999) é uma ginasta artística brasileira bicampeã olímpica e a maior medalhista da história do Brasil nas Olimpíadas, com 6 medalhas (2 ouros, 3 pratas e 1 bronze).[1] Foi campeã olímpica do salto nos Jogos de 2020 e do solo nos de 2024, vice-campeã olímpica do individual geral duas vezes consecutivas (2020, 2024), vice-campeã olímpica do salto em 2024 e bronze na competição por equipes, com a Seleção Brasileira, também em 2024. Ainda foi bicampeã mundial no salto (2021, 2023) e campeã mundial individual geral de 2022.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginasta ginastica ouro"
+    },
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal (São Paulo, 5 de abril de 2008), também conhecida como Fada, é uma skatista brasileira, medalhista olímpica de bronze nos Jogos Olímpicos de Tóquio 2020 na categoria street. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza (Rio de Janeiro, 23 de março de 2000) é uma judoca brasileira, medalhista de ouro nos Jogos Olímpicos de Paris 2024 na categoria leve. Sua conquista histórica garantiu a primeira medalha de ouro para o Brasil no judô em Jogos Olímpicos.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca"
+    }
+];
